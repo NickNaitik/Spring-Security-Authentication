3ac8135ff3b94747a6afceae32ff8e244a00b3ab
Nick Naitik <naitikyadav110@gmail.com>
2023-10-24 11:31:16 +0530

    Added dependency in pom for JJWt run at runtime.

--- src/main/java/com/nick/api/config/AppConfig.java
+++ src/main/java/com/nick/api/config/AppConfig.java
@@ -3,7 +3,6 @@ package com.nick.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,9 +10,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.Collection;
 
 @Configuration
 public class AppConfig {
